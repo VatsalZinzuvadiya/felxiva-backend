a3b943ed01718767a018feb73dd34179832b17d4
VatsalZinzuvadiya <prajapativatsal1072@gmail.com>
2024-08-17 18:08:49 +0530

    allowedOrigins added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,5 +4,9 @@ const allowedOrigins = [
     'http://127.0.0.1:3001',
     'https://flexivas.com',
     'https://flexiva-two.vercel.app',
+    'https://flexiva-three.netlify.app',
+    "https://flexiva-three.vercel.app",
+    "https://flexiva-four.netlify.app",
+    "https://flexiva-four.vercel.app"
 ]
 module.exports = allowedOrigins
--- config/corsConfigs.js
+++ config/corsConfigs.js
@@ -5,7 +5,7 @@ const corsConfigs = {
     origin: (origin, callback) => {
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             // remove ||!origin to block postman request
-
+            
             callback(null, true)
         } else {
             callback(new Error('origin not allowed by Cors'))
